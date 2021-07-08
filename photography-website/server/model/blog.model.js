9f7b196f52960a33b8025d6e55e5ff3b0a41cab0
xenodochy23 <79444246+xenodochy23@users.noreply.github.com>
2021-07-07 22:33:49 -0700

    building my back end

---
+++ photography-website/server/model/blog.model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+
+const BlogSchema = new mongoose.Schema({
+        title: {type: String},
+        description: {type: String},
+        coverArt: {type: String}
+        
+},{timestamps: true});
+
+
+module.exports = mongoose.model("Blogs", BlogSchema);
