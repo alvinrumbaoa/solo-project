be8537bca35613508f1293337f7e8b4ffca904a4
xenodochy23 <79444246+xenodochy23@users.noreply.github.com>
2021-07-17 19:21:29 -0700

    able to view blog post

--- blog/client/src/views/ViewOneBlog.js
+++ blog/client/src/views/ViewOneBlog.js
@@ -1,15 +1,35 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import  Navbar from "../components/Navbar";
+import {motion } from 'framer-motion';
+import {animationOne, transition} from "../animations/Animation";
+import axios from 'axios';
+import {Link} from '@reach/router'
 
 const ViewOneBlog = (props) =>{
+  const [blog, setBlog] = useState({});
 
+    useEffect(() =>{
+        axios.get("http://localhost:8000/api/blogs/" + props.id)
+            .then(res =>{
+                console.log(res.data);
+                setBlog(res.data)
+            })
+            .catch((err) =>{
+                console.log(err)
+            })
 
+    }, [props.id])
     return (
         <div className="view-blog-container">
-                    <Navbar/>
+              <Navbar/>
                   <motion.div initial="out" animate="in" exit="out" variants={animationOne} transition={transition}>
-                
-                </motion.div>
-
+                  <div className="blog-content">
+                  <img src={blog.coverArt} alt="big-cover-art"/>
+                  <h1 className="big-text">{blog.title}</h1>
+                  <h2 className="sub-text">{blog.description}</h2>
+                  <h3 className="info">{blog.content}</h3>
+                  </div>
+                  </motion.div>
         </div>
     )
 }
