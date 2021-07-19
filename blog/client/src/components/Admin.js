e35d3cbb02636c9d5cf5a87afa0adf2ea81c3516
xenodochy23 <79444246+xenodochy23@users.noreply.github.com>
2021-07-19 07:59:54 -0700

    made dashboard to manage data crud

--- blog/client/src/components/Admin.js
+++ blog/client/src/components/Admin.js
@@ -2,13 +2,54 @@ import React ,{useState, useEffect} from 'react';
 import Navbar from '../components/Navbar';
 import axios from 'axios';
 import {motion } from 'framer-motion';
-import {animationOne, transition} from "../animations/Animation";
-import { Link, navigate} from '@reach/router';
+import { navigate} from '@reach/router';
+import DeleteImage from './DeleteImage';
+import DeleteBlogs from './DeleteBlogs';
+import DeleteCustomer from './DeleteCustomer';
+
 const Admin = (props) =>{
         const [imageUrl, setImgUrl] = useState("");
-        
+        const [blogs, setBlogs]   =useState([]);
+        const [title, setTitle] = useState("");
+        const[image, setImage] =useState([])
+        const [description, setDescription] = useState("");
+        const [content, setContent] = useState("");
+        const [coverArt, setCoverArt] = useState("");
+        const [customer, setCustomer]   =useState([]);
 
-        const handleSubmit =(e) =>{
+        useEffect(() => {
+
+                axios.get("http://localhost:8000/api/blogs")
+                .then((res) =>{
+                        console.log(res.data);  
+                        setBlogs(res.data);  
+                })
+                .catch(err => { 
+                        console.log(err.response);
+                })             
+
+                axios.get("http://localhost:8000/api/customers")
+                .then((res) =>{
+                        console.log(res.data);  
+                        setCustomer(res.data);  
+                })
+                .catch(err => { 
+                        console.log(err.response);
+                })     
+                axios.get("http://localhost:8000/api/images")
+                .then((res) =>{
+                        console.log(res.data);  
+                        setImage(res.data);  
+                })
+                .catch(err => { 
+                        console.log(err.response);
+                })              
+        },[])
+
+
+
+
+        const ImagehandleSubmit =(e) =>{
                 e.preventDefault();
                         axios.post("http://localhost:8000/api/images", {
                         imgURL: imageUrl,
@@ -21,34 +62,110 @@ const Admin = (props) =>{
                         console.log(err.response);
                 })     
         }
+        const BloghandleSubmit =(e) =>{
+                e.preventDefault();
+                        axios.post("http://localhost:8000/api/blogs", {
+                        title: title,
+                        description:description,
+                        content: content,
+                        coverArt: coverArt,
+                })
+                .then((res) =>{
+                        console.log(res.data);    
+                        navigate("/home")
+                })
+                .catch(err => { 
+                        console.log(err.response);
+                })     
+        }
+
+
+        const BlogAfterDelete =(deletedID) =>{
+                let  filterStateArray = blogs.filter((blogs) =>{
         
+                return blogs._id !== deletedID;
+                });
+                setBlogs(filterStateArray);
+        };
+
+        const ImageAfterDelete =(deletedID) =>{
+                let  filterStateArray = image.filter((image) =>{
+        
+                return image._id !== deletedID;
+                });
+                setImage(filterStateArray);
+        };
+
+        const CustomerAfterDelete =(deletedID) =>{
+                let  filterStateArray = customer.filter((customer) =>{
+                        
+                return customer._id !== deletedID;
+                });
+                setCustomer(filterStateArray);
+        };
 
         return(
         <div className="homepage-container">
                 <div className="portfolio-container">
                         <Navbar/>
                                 <motion.div animate={{ scale: 1 }} transition={{ duration: 0.5 }}>
-                                        <h1 className="big-text">Admin</h1>     
+                                        <h1 className="big-text">Dashboard</h1>     
                                         <div className="category-container">           
                                                 <div className="cards-admin" >
-                                                                <h2>Blog</h2>
-                                                                <button>Add Blog</button>
-                                                                <button>Delete Blog</button>
-                                                                <button>Edit Blog</button>
+                                                                <h2>Blog Posts</h2>
+                                                                {
+                                                                        blogs.map((getBlog, index) =>(
+                                                                        <div className="view" key={index}>
+                                                                                {getBlog.id}
+                                                                                <h2> Title: {getBlog.title}</h2>    
+                                                                                <p> Decription: {getBlog.description}</p>    
+                                                                                <DeleteBlogs id={getBlog._id } afterDelete= {BlogAfterDelete}  />
+                                                                        </div>
+                                                                ))
+                                                                }     
+                                                                <h3>Add Blog</h3>
+                                                                <form className ="form-container"onSubmit ={ BloghandleSubmit }>  
+                                                                <p>Title</p><input type="text" name="title" onChange ={(e) => setTitle(e.target.value)}/>  
+                                                                <p>Description</p><input type="text" name="description" onChange ={(e) => setDescription(e.target.value)}/>  
+                                                                <p>Content</p><input type="text" name="content" onChange ={(e) => setContent(e.target.value)}/>  
+                                                                <p>Cover Art</p><input type="text" name="coverArt" onChange ={(e) => setCoverArt(e.target.value)}/>  <br/>
+                                                                <button type="submit">Add a Blog</button>
+                                                                
+                                                                </form>
                                                 </div>
                                                 <div className="cards-admin" >
-                                                                <h2>View Customer</h2>
-                                                                <button>View Customer</button>
-                                                                <button>DeleteCustomer</button>
+                                                                <h2>New Customer</h2>
+                                                                {
+                                                                customer.map((getCustomer, index)=>(
+                                                                        <div className="view" key={index}>                  
+                                                                        <h2>Name:   {getCustomer.name}</h2> 
+                                                                        <p>Email:</p>    {getCustomer.email}
+                                                                        <p>Message:</p>      {getCustomer.message}
+                                                                        <p>Themes:</p>     {getCustomer.theme}
+                                                                        <DeleteCustomer  id={getCustomer._id } afterDelete= {CustomerAfterDelete}   />
+                                                                        </div>
+                                                
+                                                                ))
+                                                                }
                                                 </div>                                    
                                                 <div className="cards-admin">
+                                                
                                                                 <h2>Upload Images</h2>
-                                                                <form className ="form-container"onSubmit ={ handleSubmit }>  
+                                                                <form className ="form-container"onSubmit ={ ImagehandleSubmit }>  
                                                                 <label>Image Url: </label>
                                                                 <input type="text" name="imageURL" onChange ={(e) => setImgUrl(e.target.value)}/>  
                                                                 <button type="submit">Upload</button>
                                                                 </form>
-                                                                <button>DeleteImage</button>
+                                                                {
+                                                                        image.map((image, index) => (
+                                                                                <div className="view-image" key={index}>        
+                                                                                <img src={image.imgURL} alt="images" width="200" height="200"/>
+                                                                                
+                                                                                <DeleteCustomer  id={image._id } afterDelete= {ImageAfterDelete} />
+                                                                                </div> 
+                                                                        ))
+                                                                }
+                                                                
                                                                 </div>
                                         </div>
                                 </motion.div>
