f01bba946848149bb4f5fef7027e86db4ffed776
xenodochy23 <79444246+xenodochy23@users.noreply.github.com>
2021-07-26 19:38:23 -0700

    confimation and main page

---
+++ template-react/client/src/views/Confirmation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const MainPage = ( )=>{
+
+    return(
+        <div className="container`">
+            <nav>
+                <img className="d2w-logo"src="D2W.PNG"  alt="d2w-logo"/>
+            </nav>  
+                <h1></h1>
+        </div>
+    )
+
+}
+
+
+export default Confirmation;
--- template-react/client/src/views/MainPage.js
+++ template-react/client/src/views/MainPage.js
@@ -3,19 +3,61 @@ import React from 'react';
 
 const MainPage = ( )=>{
 
-    const[darkMode, setDarkMode] = ("container");
-
-
-    const darkModeToggle = () =>{  
-        darkMode === "dark-mode" ? setDarkMode("dark-mode") : setDarkMode("container");
-    }
-
+ 
     return(
-        <div className={darkMode} >
-        
-        <button onClick={darkModeToggle}>Dark Mode</button>
-
-
+        <div className="container" >
+        <nav>
+                <img className="d2w-logo"src="D2W.PNG"  alt="d2w-logo"/>
+        </nav>  
+            <div className="box-dashboard-2">
+                 <table className="table-wrapper">
+                    <tr>
+                        <th>Date</th>
+                        <th>Status</th>
+                        <th>Port</th>
+                        <th>Destination</th>
+                        <th>FEU</th>
+                        <th>Spec</th>
+                        <th>Inquire</th>
+                    </tr>
+                    <tr>
+                        <td>08/2/2021</td>
+                        <td>Available</td>
+                        <td>HOU</td>
+                        <td>NY/NJ</td>
+                        <td>30</td>
+                        <td>Ref</td>
+                        <td><button>Inquire</button></td>
+                    </tr>
+                    <tr>
+                        <td>08/2/2021</td>
+                        <td>Available</td>
+                        <td>HOU</td>
+                        <td>NY/NJ</td>
+                        <td>30</td>
+                        <td>Ref</td>
+                        <td><button>Inquire</button></td>
+                    </tr>
+                    <tr>
+                        <td>08/2/2021</td>
+                        <td>Available</td>
+                        <td>HOU</td>
+                        <td>NY/NJ</td>
+                        <td>30</td>
+                        <td>Ref</td>
+                        <td><button>Inquire</button></td>
+                    </tr>
+                    <tr>
+                        <td>08/2/2021</td>
+                        <td>Available</td>
+                        <td>HOU</td>
+                        <td>NY/NJ</td>
+                        <td>30</td>
+                        <td>Ref</td>
+                        <td><button>Inquire</button></td>
+                    </tr>
+                    </table>    
+            </div>
         </div>
     )
 }
