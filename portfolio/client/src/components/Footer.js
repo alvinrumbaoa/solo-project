75708575866c2fefa183fd6055ca3af76cf9557c
xenodochy23 <79444246+xenodochy23@users.noreply.github.com>
2021-08-04 23:32:21 -0700

    portfolio

---
+++ portfolio/client/src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Navbar = () =>{
+
+
+
+    return(
+        <div>
+            <h1>Logo here</h1>
+
+            <ul>
+                <li>About</li>
+                <li>My Works</li>
+                <li>Contact Me</li>
+            </ul>
+        </div>
+    )
+}
+
+
+export default Navbar;
---
+++ portfolio/client/src/components/MyWorks.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const MyWorks = () =>{
+
+
+
+    return(
+        <div>
+            <h1>Logo here</h1>
+
+            <ul>
+                <li>About</li>
+                <li>My Works</li>
+                <li>Contact Me</li>
+            </ul>
+        </div>
+    )
+}
+
+
+export default MyWorks;
