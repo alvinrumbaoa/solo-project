ced5bf8edae6e4ce331aa3004341a8113d6e14e5
xenodochy23 <79444246+xenodochy23@users.noreply.github.com>
2021-08-06 02:32:15 -0700

    with tailwind css

--- portfolio/client/src/components/Home.js
+++ portfolio/client/src/components/Home.js
@@ -5,28 +5,37 @@ const Home = () =>{
 
 
     return(
-        <div>
-            <h1>Im Alvin Rumbaoa</h1>
-            <h3>I'm a Full Stack Developer</h3>
-            <Avatar
-                avatarStyle='Circle'
-                topType='ShortHairShortWaved'
-                accessoriesType='Blank'
-                hairColor='BrownDark'
-                facialHairType='Blank'
-                clotheType='BlazerSweater'
-                eyeType='Happy'
-                eyebrowType='Default'
-                mouthType='Smile'
-                skinColor='Brown'
-                />
+        <div className="bg-gray-500 p-12">
+              <div className="bg-gray-100 p-12 ">
+                 <h1>Im Alvin Rumbaoa</h1>
+                    <h3>I'm a Full Stack Developer</h3>
+                    <Avatar
+                        avatarStyle='Circle'
+                        topType='ShortHairShortWaved'
+                        accessoriesType='Blank'
+                        hairColor='BrownDark'
+                        facialHairType='Blank'
+                        clotheType='BlazerSweater'
+                        eyeType='Happy'
+                        eyebrowType='Default'
+                        mouthType='Smile'
+                        skinColor='Brown'
+                        />
+
+              </div>
+        
             <div>
                 <h2>Tech used</h2>
                 <p>Fundamentals</p>
                 <p>Front End</p>
                 <p>Back End</p>
             </div>
-            <div></div>
+            <div>
+                parralax 1
+            </div>
+            <div>
+                parralax 2
+            </div>
         </div>
     )
 }
