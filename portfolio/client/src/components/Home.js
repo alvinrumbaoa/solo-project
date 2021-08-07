2e84bd952c23a8639f9677a7b6b0869bdbe53d19
xenodochy23 <79444246+xenodochy23@users.noreply.github.com>
2021-08-07 12:36:22 -0700

    tailwind

--- portfolio/client/src/components/Home.js
+++ portfolio/client/src/components/Home.js
@@ -5,24 +5,27 @@ const Home = () =>{
 
 
     return(
-        <div className="bg-gray-500 p-12">
-              <div className="bg-gray-100 p-12 ">
-                 <h1>Im Alvin Rumbaoa</h1>
-                    <h3>I'm a Full Stack Developer</h3>
-                    <Avatar
-                        avatarStyle='Circle'
-                        topType='ShortHairShortWaved'
-                        accessoriesType='Blank'
-                        hairColor='BrownDark'
-                        facialHairType='Blank'
-                        clotheType='BlazerSweater'
-                        eyeType='Happy'
-                        eyebrowType='Default'
-                        mouthType='Smile'
-                        skinColor='Brown'
-                        />
-
-              </div>
+        <div className="bg-gray-500 p-0 mx-auto ">
+                <div className="bg-gray-100 p-24 w-120 ">
+                    <h1 className="text-left text-xl font-bold font-Poppins">Im Alvin Rumbaoa</h1>
+                        <h3 className="isolate md:isolation-auto">I'm a Full Stack Developer</h3>
+                        <button>CV</button>
+                        <div className="object-right">
+                        <Avatar
+                                    avatarStyle='Circle'
+                                    topType='ShortHairShortWaved'
+                                    accessoriesType='Blank'
+                                    hairColor='BrownDark'
+                                    facialHairType='Blank'
+                                    clotheType='BlazerSweater'
+                                    eyeType='Happy'
+                                    eyebrowType='Default'
+                                    mouthType='Smile'
+                                    skinColor='Brown'
+                                    />
+                        </div>
+                            
+            </div>
         
             <div>
                 <h2>Tech used</h2>
--- portfolio/client/src/components/Navbar.js
+++ portfolio/client/src/components/Navbar.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import {Link} from '@reach/router';
+
 
 const Navbar = () =>{
 
 
 
     return(
-        <div>
-        <h1>Alvin Rumbaoa</h1>
-
-        <ul>
-            <li>Home</li>
-            <li>About Me</li>
-            <li>My Works</li>
-            <li>Contact Me</li>
-            <button>Hire Me</button>
-        </ul>
+        <div className="bg-gray-100 inline-flex space-x-0 h-6  md:w-auto ">
+                <h1>Alvin Rumbaoa</h1>
+                <ul className ="py-1 inline-flex space-x-4">
+                    <li>Home</li>
+                    <li>About Me</li>
+                    <li>My Works</li>
+                    <li>Contact Me</li>
+                    <button className =" focus:ring-2  focus:ring-red-500 bg-red-500 hover:bg-red-700 ">Hire Me</button>
+                    
+                </ul>
     </div>
     )
 }
