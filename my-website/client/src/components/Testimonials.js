94b1f5c937a805ff6b0513c4339b608908b57274
xenodochy23 <79444246+xenodochy23@users.noreply.github.com>
2021-08-10 00:21:22 -0700

    skills and testimonials section with images

---
+++ my-website/client/src/components/Testimonials.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Avatar from 'avataaars';
+
+const Testimonials = (props) => {
+
+    return(
+        <div className= "container">
+            <div
+                <h1>Testimonials</h1>
+        </div>
+    )
+}
+
+
+export default Testimonials;
